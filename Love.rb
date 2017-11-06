f7bcc6cf0f41271334ce380343a2fbd3759d9c73
David Allen <deallen@Davids-MacBook-Pro.local>
2017-11-06 11:53:18 -0500

    initial commit of the date scheduler I made my girl friend for Christmas 2016

---
+++ Love.rb
@@ -0,0 +1,53 @@
+puts "Hi, Sami. Merry Christmas! It's me! David. I downloaded just a smidgen of my brain into your computer. Cool huh?"
+gets.chomp
+
+puts "I hope you are having a great day. I've been looking forward to our first Christmas Day together for a loooooooong time. Haven't you?"
+gets.chomp
+
+puts "Me too, me too XD. Now before we get really mushy-gushy and start kissing computer screens, lets get this Christmas gift thing settled! So what do you say? Are you interested in playing a Christmas gift game?"
+first_response = gets.chomp
+
+puts "'#{first_response}', eh? Haha you are a cutie. This will be fun! It's going to be fun because there is still so much to decide! Depending on yours responses to a few questions, this program is going to design the perfect date night for you and me. Ready?"
+second_response = gets.chomp
+
+puts "'#{second_response}'! Hahah, you slay me. Ok, let's dive in! What is your faaaaaaaavorite night for a datenight? Do you like Thursday, Fridays, Saturdays, Sundays? Or maybe you are interested in a Monday night date. What do you say?" 
+date_night = gets.chomp
+
+puts "Ok! #{date_night} it is! What fun. Now, I'm actually going to be in charge of the timing of our date, because I'd like us to start earlish in the evening. I want to walk with you around the battery and the beautiful homes before we go to pre-dinner drinks. Sound good?"
+gets.chomp
+
+puts "Great! :) So to summarize, we'll aim for #{date_night} night, and we'll plan on taking a romantic jaunt around the Battery first. Second question: What is THE hot restaurant in Charleston right now? You've mentioned a few to me. I'm sparing no expense on this meal. What would you enjoy eating the most!? There is Husk and Ches Nous and of course that cool little Italian spot we ate at with my parents. There are new places like Scarecrow and Feathertop and goat.sheep.cow.north. Think about it for a second."
+sleep(10)
+puts "So what do you think? Where should we eat?"
+dinner_date = gets.chomp
+
+puts "Amazing! We'll get reservations for #{date_night} night at #{dinner_date}. Sound like a plan so far?"
+gets.chomp
+
+puts "Super doper! Now, let's kick it up a notch. Drinks and dessert, and then extracurricular activities. You wouldn't be interested in a pre-dinner drink at a place like 492 would you? Maybe the Cocktail Club? We could even do the Belmont or Proof or the Bar at Husk. So many options!! What do you think?"
+bar_preference = gets.chomp
+
+puts "Ok! Haha great. We'll plan to do pre-dinner drinks #{date_night} at #{bar_preference} and then we'll go to #{dinner_date} for dinner. Sounding fun so far?"
+gets.chomp
+
+puts "I know! We always have so much fun together. I'm excited too."
+
+puts "Now how about dessert? It'd be fun to go to a dessert bar wouldn't it? Because we'll definitely want to get moving after our main course. I'd fancy a walk after dinner on King Street. So what do you think? What would be a good dessert/after-dinner-drinks place?"
+dessert = gets.chomp
+
+puts "Sounds fun :) We'll walk to #{dessert} after dinner."
+sleep (5)
+
+puts "So to summarize our evening so far, we'll plan our date on #{date_night}. We'll grab drinks early at #{bar_preference}. And then we'll go to #{dinner_date}! And theeeen we'll walk to #{dessert}. Oh, what fun what fun! I was thinking that we should also probably see Star Wars together. But maybe that's not on this date night. Maybe that is a bonus date that you just got yourself. Bonus date sound good?"
+bonus_date = gets.chomp
+
+puts "Sounds good to me! Ok, so would you say that you are excited?"
+gets.chomp
+
+puts "REALLY excited or just kind of excited?"
+gets.chomp
+
+puts "Good! I like to keep my lady smiling :D. That's enough date-night planning. Let's move on to some loooooooove. We've been through an awful lot together Sami, and I'm very excited to continue growing and learning about this crazy world that we live in. Here's to an adventurous (and very well-traveled) life! To life to life! L'chaim! Let's enjoy the rest of our Christmas and celebrate a great 2016, and an exciting 2017."
+gets.chomp
+puts "Hit 'enter one more time for this program to end'. It's been fun Sami :) I'm looking forward to our date night and another year together."
+
